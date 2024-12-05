777f0884ca3d993bd0d78c0c75ee123cbe10040b
Alex Massen-Hane <alex_mh23@outlook.com>
2024-12-05 18:08:27 +0800

    Day 1 - Update

--- Day 1/main.py
+++ Day 1/main.py
@@ -3,25 +3,14 @@ with open("input.txt", "r") as file:
 
 cleaned = [line.rstrip().split("   ") for line in raw_data]
 
-x = []
-y = []
-for line in cleaned:
-    x.append(int(line[0]))
-    y.append(int(line[1]))
+x = [int(line[0]) for line in cleaned]
+y = [int(line[1]) for line in cleaned]
+
+x.sort()
+y.sort()
 
 total_sum = 0
-done = 0
-for i in range(len(x)):
-    x_min_loc = x.index(min(x))
-    y_min_loc = y.index(min(y))
-
-    diff = abs(x[x_min_loc] - y[y_min_loc])
-    total_sum += diff
-    done += 1
-
-    print(diff, total_sum, x_min_loc, y_min_loc, len(x), done)
-
-    x.pop(x_min_loc)
-    y.pop(y_min_loc)
+for xx, yy in zip(x, y):
+    total_sum += abs(xx - yy)
 
 print(f"Answer: {total_sum}")
