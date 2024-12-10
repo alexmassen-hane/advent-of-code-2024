1eb2b86636b83ab44b78d8b9ebf1a94398f189aa
Alex Massen-Hane <alexmh23@outlook.com>
2024-12-11 01:22:47 +0800

    Add path dictionary

---
+++ Day 6/part1_path_dict.py
@@ -0,0 +1,87 @@
+def find_start_position(map):
+    """Find the starting position of the guard, marked by the '^' character."""
+    for j in range(len(map[0])):
+        for i in range(len(map[j])):
+
+            if map[j][i] == "^":
+                return (i, j)
+
+    return (None, None)
+
+
+def update_direction(dir):
+    """Rotate the guard's direction using a -90 deg rotation matrix"""
+    return (0 * dir[0] + 1 * dir[1], -1 * dir[0] + 0 * dir[1])
+
+
+def is_in_bounds(data, pos):
+    """Check if the position of the guard is within the bounds of the grid."""
+    return (
+        pos[0] <= len(data[0]) - 1
+        and pos[0] >= 0
+        and pos[1] <= len(data) - 1
+        and pos[1] >= 0
+    )
+
+
+def display_map(data, path):
+
+    print("---------------------------------------")
+    for pos, _ in path.items():
+        # print(pos)
+        data[pos[1]][pos[0]] = "X"
+
+    for line in data:
+        print("".join(line))
+    print("---------------------------------------")
+
+
+# Read in the map data from file
+with open("ex_input.txt", "r") as file:
+    data = [[char for char in line.rstrip()] for line in file]
+
+# Find the initial position of the guard.
+pos = find_start_position(data)
+dir = (0, 1)
+
+print(f"Initial position and direction: {pos}, {dir}")
+path = {}
+
+object_in_path = True
+while object_in_path:
+
+    # If it's within bounds of map and no object in path found yet.
+    object_in_path = False
+    while is_in_bounds(data, pos) and not object_in_path:
+
+        # Check if the point we're trying is an obstacle.
+        if data[pos[1]][pos[0]] == "#":
+            object_in_path = True
+
+            # Debugging
+            print(f"Found obstical at: {pos}")
+            display_map(data, path)
+
+            last_pos = (pos[0] - dir[0], pos[1] + dir[1])
+
+            # Rotate the direction of the guard
+            dir = update_direction(dir)
+
+        # Keep track of the path that the guard has taken
+        if not pos in path and not object_in_path:
+            path[pos] = None
+
+        # Update the position for next iteration.
+        pos = (pos[0] + dir[0], pos[1] - dir[1])
+
+    if not object_in_path:
+        object_to_hit = False
+
+    # Update old position to new
+    pos = last_pos
+
+
+# Debugging
+display_map(data, path)
+
+print(f"Answer: {len(path)}")
