a6356765409018f2502761771f4afea0c6a294f8
Alex Massen-Hane <alexmh23@outlook.com>
2024-12-12 22:43:47 +0800

    Day 4 part 2 complete

---
+++ Day 4/part2_hard_and_dumb_way.py
@@ -0,0 +1,44 @@
+import re
+
+
+def rotate_matrix(data):
+    return [list(row) for row in zip(*data[::-1])]
+
+
+with open("input.txt", "r") as file:
+    data = [list(line.rstrip()) for line in file.readlines()]
+
+# data = [
+#     ["1", "2", "3"],
+#     ["M", "b", "S"],
+#     ["d", "A", "f"],
+#     ["M", "h", "S"],
+#     ["4", "5", "6"],
+# ]
+
+# Rotate the matrix 3 times
+x_count = 0
+for rotate in range(1, 5):
+
+    for i, line in enumerate(data[1:-1]):
+
+        # Find all the A's in the row thats not next to the edges
+        indexes = [
+            match.start()
+            for match in re.finditer(r"A", "".join(line))
+            if match.start() > 0 and match.start() < len("".join(line)) - 1
+        ]
+
+        # Scan along where the As were found
+        for j in indexes:
+
+            # Check both diagonal directions and add to count if works.
+            check_down = "".join([data[i][j - 1], data[i + 1][j], data[i + 2][j + 1]])
+            check_up = "".join([data[i + 2][j - 1], data[i + 1][j], data[i][j + 1]])
+            if re.findall(r"MAS", check_down) and re.findall(r"MAS", check_up):
+                x_count += 1
+
+    # Rotate the matrix for next iteration
+    data = rotate_matrix(data)
+
+print(f"Answer: {x_count}")
